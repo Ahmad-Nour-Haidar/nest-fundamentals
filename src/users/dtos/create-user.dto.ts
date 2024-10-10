489d391f6e065adbaa4009df15d43f0b6bba17f5
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-10-10 14:04:46 +0300

    ✨ 21) Nestjs_ Validate Input data in DTO

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @Length(3, 255)
   username: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @Length(2, 255)
   country: string;
 }
